9d58cbdd42b753982b3b647c5404cd80c0e5d412
lauraaromano <lauraromano2018@gmail.com>
2026-06-09 15:00:21 -0300

    feat: aula 03 – Atividade Prática

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ class ComponenteSchema(BaseModel):
     nome: str = Field(..., min_length=2, description="Nome do componente maker")
     quantidade: int = Field(..., ge=0, description="Quantidade em estoque (deve ser maior ou igual a zero)")
     categoria: str = Field(..., description="Categoria do item (ex: Atuadores, Microcontroladores)")
+    estado: str = Field(default="Disponível", description="Estado do componente (ex: Disponível, Em Uso, Defeituoso)")
+
 
 # 3. Nosso "Banco de Dados" temporário em memória
 estoque_laboratorio = [
-    {"id": 1, "nome": "Arduino Sensor Shield", "quantidade": 15, "categoria": "Placas de Expansão"},
-    {"id": 2, "nome": "Micro Servo Motor SG90", "quantidade": 42, "categoria": "Atuadores"},
-    {"id": 3, "nome": "Esteira em Acrílico", "quantidade": 2, "categoria": "Mecânica"}
+    {"id": 1, "nome": "Arduino Sensor Shield", "quantidade": 15, "categoria": "Placas de Expansão", "estado": "Disponível"},
+    {"id": 2, "nome": "Micro Servo Motor SG90", "quantidade": 42, "categoria": "Atuadores", "estado": "Disponível"},
+    {"id": 3, "nome": "Esteira em Acrílico", "quantidade": 2, "categoria": "Mecânica", "estado": "Disponível"},
+    {"id": 4, "nome": "Sensor Ultrassônico HC-SR04", "quantidade": 30, "categoria": "Sensores", "estado": "Disponível"}
 ]
 
 # Rota Raiz
@@ -26,6 +29,7 @@ def raiz():
 @app.get("/componentes")
 def listar_componentes():
     return estoque_laboratorio
+    raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
 
 # CRUD - CREATE (Cadastrar novo item)
 @app.post("/componentes", status_code=201)
@@ -43,6 +47,7 @@ def adicionar_componente(novo_componente: ComponenteSchema):
     
     estoque_laboratorio.append(componente_dict)
     return {"mensagem": "Componente adicionado com sucesso!", "componente": componente_dict}
+    raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
 
 # CRUD - UPDATE (Atualizar quantidade ou dados)
 @app.put("/componentes/{componente_id}")
@@ -52,6 +57,7 @@ def atualizar_componente(componente_id: int, dados_atualizados: ComponenteSchema
             item["nome"] = dados_atualizados.nome
             item["quantidade"] = dados_atualizados.quantidade
             item["categoria"] = dados_atualizados.categoria
+            item["estado"] = dados_atualizados.estado
             return {"mensagem": "Componente atualizado com sucesso!", "componente": item}
             
     raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
